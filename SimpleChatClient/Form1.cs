a3dac38c38ca7b56d99509f4464a26cabd684adb
Rex Zeng <rex.fgtsky@qq.com>
2015-12-17 17:30:18 +0800

    完成界面，交互待定

--- SimpleChatClient/Form1.cs
+++ SimpleChatClient/Form1.cs
@@ -16,5 +16,26 @@ namespace SimpleChatClient
         {
             InitializeComponent();
         }
-    }
+
+		private void btnNewChat_Click(object sender, EventArgs e)
+		{
+			frmInput input_form = new frmInput();
+			if (input_form.ShowDialog(this) == DialogResult.OK)
+			{
+				try
+				{
+					String server = input_form.serverInfo();
+					String nickname = input_form.nickname();
+					// 这里链接服务器
+					//
+					lblServerInfo.Text = "当前聊天服务器：" + server;
+					lblUserInfo.Text = "当前昵称：" + nickname;
+				}
+				catch
+				{
+					MessageBox.Show("设置失败:(");
+				}
+			}
+		}
+	}
 }
